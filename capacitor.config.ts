d63bd53e9c600e9a9e7f9f751e8dec01457b02a1
Bimbo Escodero Villaganas <villaganasbimbo143@gmail.com>
2024-06-04 02:32:13 +0800

    second commit

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
-  appName: 'ionic-t-villaganas',
+  appName: 'foryou',
   webDir: 'dist',
   server: {
     androidScheme: 'https'
